dff8e8ae6a31b8e20e1e8690df337441eb59f055
科诺设计\科诺设计 <1402992668@qq.com>
2017-01-07 01:27:13 +0800

    no message

--- routes/web.php
+++ routes/web.php
@@ -4,10 +4,9 @@
 |--------------------------------------------------------------------------
 | Web Routes
 |--------------------------------------------------------------------------
-|
-| This file is where you may define all of the routes that are handled
-| by your application. Just tell Laravel the URIs it should respond
-| to using a Closure or controller method. Build something great!
+|	配置网站全局路由
+| 	by:king
+| 	Email:1402992668@qq.com
 |
 */
 
